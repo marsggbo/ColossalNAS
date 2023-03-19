5e48b36f46c8bc86f219aeb1ea4643cf22ae9c91
marsggbo <1435679023@qq>
2023-03-19 03:46:48 -0700

    update

--- zero/run.sh
+++ zero/run.sh
@@ -5,5 +5,8 @@ rm /lib/x86_64-linux-gnu/libcuda.so.1
 ln -s /lib/x86_64-linux-gnu/libcuda.so.5* /usr/lib/x86_64-linux-gnu/libcuda.so.1
 rm /lib/x86_64-linux-gnu/libnvidia-ml.so.1
 ln -s /lib/x86_64-linux-gnu/libnvidia-ml.so.5* /lib/x86_64-linux-gnu/libnvidia-ml.so.1
+cd /workspace/TensorNVMe
+pip uninstall tensornvme
+DISABLE_URING=1 pip install -v --no-cache-dir .
 cd /workspace/colossalnas/zero
 bash ./scripts/batch_benchmark.sh
